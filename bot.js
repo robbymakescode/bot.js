01b0d60fbba9cc95a6e851d3c7468d0ef31577b4
robbymakescode <92763053+robbymakescode@users.noreply.github.com>
2021-12-13 22:48:30 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,19 @@
+/*
+ * Ur mom gae
+ * Hi
+ * Deez nuts
+ */
+
+const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
+
+if(!process.send) {
+
+Files.initStandalone();
+
+} else {
+
+process.on('message', function(content) {
+	Files.initBotTest(content);
+});
+
+}
